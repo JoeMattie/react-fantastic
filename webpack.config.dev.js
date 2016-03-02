fbcc8cd5da23a2c49c770e89c3ada09aea1f642f
JoeMattie <joemattie@gmail.com>
2016-03-02 08:55:14 +0000

    super dumb

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,8 @@
-const webpack = require('webpack');
+import path from 'path';
+
+import webpack from 'webpack';
+const node_modules = path.resolve(__dirname, 'node_modules');
+const pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 let config = {
     devtool: 'eval-source-map',
@@ -18,11 +22,14 @@ let config = {
     ],
     resolve: {
         extensions: ['', '.jsx', '.scss', '.js', '.json']
+        // alias: { 'react': pathToReact,
+        //      'react-dom': path.resolve(node_modules, 'react-dom/dist/react-dom.min.js') }
     },
     module: {
+        // noParse: [ pathToReact, path.resolve(node_modules, 'react-dom/dist/react-dom.min.js')],
         loaders: [
-            { test: /\.jsx?$/,  loader: 'babel' },
-            { test: /\.js?$/,   loader: 'babel' },
+            { test: /\.jsx?$/,  loader: 'babel', include: path.resolve(__dirname, 'client') },
+            { test: /\.js?$/,   loader: 'babel', include: path.resolve(__dirname, 'client') },
             { test: /\.css$/,   loaders: ['style','css'] },
             { test: /\.scss$/,  loaders: ['style','css', 'sass'] },
             { test: /\.gif$/,   loader: 'url-loader?mimetype=image/png' },
